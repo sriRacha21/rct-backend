72a6002c8b84c907876945667f7780e7f08a4011
Kirtan Patel <kirtanpatl@gmail.com>
2020-08-29 19:31:27 -0400

    Change up how we check year on notifications, still needs some refactoring

--- notify.py
+++ notify.py
@@ -4,6 +4,8 @@ import os
 import requests
 import json
 import threading
+import time
+import datetime
 # typing
 from typing import List
 
@@ -25,16 +27,28 @@ app = firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://rutgers-course-tracker.firebaseio.com/'
 })
 
-# 
-# res = requests.get('{}?year=2020&term=9&campus=NB'.format(baseCoursesURI))
-# classes = res.json()
+
+# Gets current main semester from the text file
+f = open("season.txt", "r")
+sem = f.readline().rsplit('\n')[0]
+f.close()
+
+# Sets the seasons according to the int value
+seasons = [9, 7] if sem == 'fall' else [1, 0];
+# year = datetime.datetime.now().year
 
 trackersSnapshot = []
 trackersSnapshotLock = False
 
-def checkNotify( db ):
+def checkNotify(db, season: int):
     # trackersSnapshot = db.collection("trackers").where("active", "==", True).get()
     trackersSnapshotLock = True
+    year = datetime.datetime.now().year
+    isSpring = season == 1
+    currentYear = year + (1 if isSpring else 0)
+
+    openSections = listOfOpenIndex(currentYear, season)
+
     for trackerDoc in trackersSnapshot:
         # get all fields
         subject = trackerDoc.get("subject")
@@ -44,20 +58,12 @@ def checkNotify( db ):
         course = trackerDoc.get("courseNumber")
         courseName = trackerDoc.get("course")
         uid = trackerDoc.get("user")
-        # query SOC
-        requestURI = f'{baseOpenSectionsURI}?year={year}&term={intSeason[semester]}&campus=NB'
-        print("Requesting URI:", requestURI)
-        # try to query the URL
-        res = None
-        try:
-            res = requests.get(requestURI)
-        except:
-            print("SOC API Connection error.")
-            continue
-        openSections = listToDict(json.loads(res.text))
+
         # guard clause for class not open
+
         if index not in openSections:
             continue
+
         # get users
         usersSnapshot = db.collection("users").where("user", "==", uid).limit(1).get()
         for userDoc in usersSnapshot:
@@ -114,12 +120,30 @@ def listToDict( strings: List[str] ) -> dict:
         dictionary[string] = True
     return dictionary
 
-callback_done = threading.Event()
+def listOfOpenIndex(year: int, semester: int) -> dict:
+    # query SOC
+    requestURI = f'{baseOpenSectionsURI}?year={year}&term={semester}&campus=NB'
+    print("Requesting URI:", requestURI)
+    # try to query the URL
+    res = None
+    try:
+        res = requests.get(requestURI)
+    except:
+        print("SOC API Connection error.")
+        trackersSnapshotLock = False
+        return
+    openSections = listToDict(json.loads(res.text))
+    return openSections
 
 # Create a callback on_snapshot function to capture changes
+
+db = firestore.client()
+callback_done = threading.Event()
+
 def on_snapshot(doc_snapshot, changes, read_time):
-    if trackersSnapshotLock:
-        return
+    while trackersSnapshotLock:
+        continue
+
     trackersSnapshot = doc_snapshot
     for doc in doc_snapshot:
         print(f'Received document snapshot: {doc.id}')
@@ -130,4 +154,9 @@ doc_ref = db.collection("trackers").where("active", "==", True)
 # Watch the document
 doc_watch = doc_ref.on_snapshot(on_snapshot)
 
-checkNotify( firestore.client() )
+while True:
+    seasons = [9, 7] if sem == 'fall' else [1, 0];
+
+    for season in seasons:
+        checkNotify(db, season)
+    time.sleep(15)
